0ef394ef13e64f566c93552ed51820ad5f280e34
Victor Hugo <victorhugodossantos7@gmail.com>
2021-09-18 16:48:12 -0300

    feat: image texture test

--- Minecraft.py
+++ Minecraft.py
@@ -4,6 +4,6 @@ def update():
         test_square.x -= 4 * time.dt
 app = Ursina()#Basic instance of the game
 test_square = Entity(model = 'quad', color = color.red, scale = (1,4), position = (5,1))
-# sans_texture = land_texture('assets/Sans.png')
-# sans = Entity(model = 'quad', texture = sans_texture)
+mc_texture = load_texture('assets/images/minecraft-icon.png')
+mc = Entity(model = 'quad', texture = mc_texture)
 app.run()#Run the instance
