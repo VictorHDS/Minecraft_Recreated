893efc8556b8e6ec6021c75d7c42e44fe8c6e627
Victor Hugo <victorhugodossantos7@gmail.com>
2021-09-18 18:09:24 -0300

    feat: inventory bottom navbar

--- Minecraft.py
+++ Minecraft.py
@@ -1,28 +1,17 @@
 from ursina import *
-class Test_cube(Entity):
-    def __init__(self):
-        super().__init__(
-            model = 'cube', 
-            color = color.white, 
-            texture = 'white_cube', 
-            rotation = Vec3(45,45,45)
-            )
-class Test_button(Button):
-    def __init__(self):
+class Voxel(Button):
+    def __init__(self, position = (0,0,0)):
         super().__init__(
             parent = scene,
+            position = position,
             model = 'cube',
-            texture = 'brick',
-            color = color.blue,
-            highlight_color = color.red,
-            press_color = color.yellow
+            origin_y = 0.5,
+            texture = 'white_cube',
+            color = color.white,
+            highlight_color = color.lime
         )
-def update():
-    if held_keys['a']:
-        test_square.x -= 4 * time.dt
-app = Ursina()#Basic instance of the game
-test_square = Entity(model = 'quad', color = color.red, scale = (1,4), position = (5,1))
-mc_texture = load_texture('assets/images/minecraft-icon.png')
-mc = Entity(model = 'quad', texture = mc_texture)
-test_cube = Test_button()
-app.run()#Run the instance
+app = Ursina()
+for z in range(8):
+    for x in range(8):
+        voxel = Voxel(position = (x,0,z))
+app.run()
