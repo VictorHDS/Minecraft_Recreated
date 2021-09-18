60b2785d2a2fac81e4810a2f0bd8e3373da56349
Victor Hugo <victorhugodossantos7@gmail.com>
2021-09-18 19:07:42 -0300

    feat: grass, stone, brick and dirt textures

--- Minecraft.py
+++ Minecraft.py
@@ -1,15 +1,21 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+app = Ursina()
+grass_texture = load_texture('assets/images/grass_block.png')
+stone_texture = load_texture('assets/images/stone_block.png')
+brick_texture = load_texture('assets/images/brick_block.png')
+dirt_texture = load_texture('assts/images/dirt_block.png')
 class Voxel(Button):
     def __init__(self, position = (0,0,0)):
         super().__init__(
             parent = scene,
             position = position,
-            model = 'cube',
+            model = 'assets/block',
             origin_y = 0.5,
-            texture = 'white_cube',
+            texture = grass_texture,
             color = color.color(0,0,random.uniform(0.9,1)),
-            highlight_color = color.lime
+            highlight_color = color.lime,
+            scale = 0.5
         )
     def input(self, key):
         if self.hovered:
@@ -17,7 +23,7 @@ class Voxel(Button):
                 voxel = Voxel(position = self.position + mouse.normal)
             if key == 'left mouse down':
                 destroy(self)
-app = Ursina()
+
 for z in range(16):
     for x in range(16):
         voxel = Voxel(position = (x,0,z))
